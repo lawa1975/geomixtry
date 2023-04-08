954c87f000ade5eb5383a641ece7f1c21f86770c
Lars Wagner <wagner1975@web.de>
2023-04-08 18:53:28 +0200

    feat: calculate circle area

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,14 +1,23 @@
-import {calcRectangleArea} from './main';
+import {calcRectangleArea, calcCircleArea} from './main';
 
-test('at least one parameter is zero', () => {
+test('calcRectangleArea with at least one parameter is zero', () => {
   expect(calcRectangleArea(0, 0)).toBe(0);
   expect(calcRectangleArea(0, 5)).toBe(0);
   expect(calcRectangleArea(5, 0)).toBe(0);
 });
 
-test('positive and negative parameters', () => {
+test('calcRectangleArea with positive and negative parameters', () => {
   expect(calcRectangleArea(5, 11)).toBe(55);
   expect(calcRectangleArea(-5, 11)).toBe(55);
   expect(calcRectangleArea(5, -11)).toBe(55);
   expect(calcRectangleArea(-5, -11)).toBe(55);  
+});
+
+test('calcCircleArea with radius is zero', () => {
+  expect(calcCircleArea(0)).toBe(0);
+});
+
+test('calcCircleArea with radius is positive or negative', () => {
+  expect(calcCircleArea(12.8)).toBeCloseTo(514.7185);
+  expect(calcCircleArea(-12.8)).toBeCloseTo(514.7185);
 });
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,3 @@
 export const calcRectangleArea = (x: number, y: number): number => Math.abs(x) * Math.abs(y);
+
+export const calcCircleArea = (radius: number): number => radius * radius * Math.PI;
